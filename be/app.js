ca2ed3cee1cb0586cd6321f006fbbeb0bd89848b
김웅기 <kwk@gim-ung-giui-MacBook-Pro.local>
2019-05-15 14:26:26 +0900

    MongoDB 설치 및 연결

--- be/app.js
+++ be/app.js
@@ -39,3 +39,51 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+
+const mongoose = require('mongoose')
+
+
+// 모델만들기
+// 1. 스키마 부터 만들자
+const userSchema = new mongoose.Schema({
+  name: {type: String, default: ''},
+  age: {type: Number, default: 1}
+})
+
+const User = mongoose.model('User', userSchema)
+
+
+// mongoose 연결
+
+mongoose.connect('mongodb://localhost:27017/nemv', { useNewUrlParser: true }, (err) => {
+  if (err) return console.log(err)
+  console.log('mongoose connected!');
+})
+
+
+// 스키마의 CRUD
+// Create
+// User.create({name: '하하'})
+//   .then(r => console.log(r))
+//   .catch(e => console.error(e))
+
+// Read
+User.find()
+.then(r => console.log(r))
+.catch(e => console.error(e))
+
+// Update
+// User.updateOne({ _id: '5cdb9c141d479ce0d9225213'}, {$set: {age: 34}}) // 얘는 Promise를 받아서 then과 catch를 뱉는다.
+// .then(r => {
+//   console.log(r)
+//   console.log('updated')
+//   return User.find()
+// })
+// .then(r => console.log(r))
+// .catch(e => console.error(e))
+
+// delete
+// User.deleteOne({name : '하하'})
+// .then(r => console.log(r))
+// .catch(e => console.error(e))
